70f038e43ab4575946b37e1dc667cb4ad5a4f561
pporschen <accounts@pp83.de>
2020-04-28 13:27:31 +0200

    test

--- src/App.js
+++ src/App.js
@@ -4,71 +4,71 @@ import './style.css';
 
 
 class App extends React.Component {
-  constructor(){
-    super();
+  constructor(props) {
+    super(props);
     this.state = {
       todo: "",
       list: []
     }
   }
 
- 
-  addItem = (e) =>{
+
+  addItem = (e) => {
     e.preventDefault();
     const todo = {
-      id: 1+Math.random(),
+      id: 1 + Math.random(),
       value: this.state.todo
     }
 
-    const list = [...this.state.list,todo]
+    const list = [...this.state.list, todo]
 
     this.setState({
       list,
-      todo:""
+      todo: ""
     })
   }
 
-  updateTodo = (key,value) =>{
-      this.setState({
-        [key] : value
-      })
+  updateTodo = (key, value) => {
+    this.setState({
+      [key]: value
+    })
   }
 
-  deleteTodo = (id) =>{
+  deleteTodo = (id) => {
     const list = [...this.state.list]
     const updateList = list.filter(item => item.id !== id)
     this.setState({
       list: updateList
     })
   }
-  
 
-render(){
-  return (
-    <div className="App">
-          <h1>The List</h1>
-          <div className="AddTodoSection">
-                <form onSubmit = {(e)=>this.addItem(e)}>
-                    <input value = {this.state.todo}  className="Input" type="text" placeholder=" What do you want to do?" 
-                    onChange = {e => this.updateTodo("todo",e.target.value)}/>
-                    </form>
-                    </div>
-                    <br/>
-                    <ul>
-                    {this.state.list.map(each =>{
-                        return (
-                        <li key = {each.id}>
-                            <input className="Itemcheckbox"  type="checkbox"/>
-                            {each.value}
-                        <button className = "Deletebutton" onClick = {() => this.deleteTodo(each.id) }>X</button>
-                        </li>
-                        )
-                    })}
-                </ul>
-                   
-       </div>
-  );
-}
+
+  render() {
+    return (
+      <div className="App">
+        <h1>The List</h1>
+        <div className="AddTodoSection">
+          <form onSubmit={(e) => this.addItem(e)}>
+            <input value={this.state.todo} className="Input" type="text" placeholder=" What do you want to do?"
+              onChange={e => this.updateTodo("todo", e.target.value)} />
+          </form>
+        </div>
+        <br />
+        <ul>
+          {this.state.list.map(each => {
+            return (
+              <li key={each.id}>
+                <input className="Itemcheckbox" type="checkbox" />
+                {each.value}
+                <button className="Deletebutton" onClick={() => this.deleteTodo(each.id)}>X</button>
+              </li>
+            )
+          })}
+        </ul>
+
+      </div>
+    );
+  }
 }
 
 export default App;
